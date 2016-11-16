f84388ae9d72f8c3d71e40ae9dd0f028974ab927
DavidCGS <shisui_clow@hotmail.com>
2016-11-16 16:15:43 -0600

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
-module.exports = {
-    "IP" : process.env.IP || '127.0.0.1',
-    "PORT" : process.env.PORT || 3030
-    // "color_theme" : {
-    //     "info":"rainbow",
-    //     "data":"green",
-    //     "error":"red"
-    // }
+module.exports = {"IP": process.env.IP || '127.0.0.1', "PORT": process.env.PORT || 3000,
+ "color_theme":{
+    
+  "info":"rainbow",
+  "data":"green",
+  "error":'zebra',
+ "warning":'yellow'
+},
+"STATIC_PATH": './static'
 };
