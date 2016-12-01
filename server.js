ee213f5acb361c4ecb56713db4fb853fddfe6137
DavidCGS <shisui_clow@hotmail.com>
2016-12-01 01:04:03 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
-var http = require('http');
-var colors = require('colors');
-var fs = require('fs');
-var mime = require('mime');
-var path = require('path');
-var staticServer = require('./Internals/static-server.js');
-var handlers = require('./Internals/handlers');
-
-//cargando configuraciones
-var config = require('./config/config.js');
-var IP = config.IP;
-var PORT = config.PORT;
-// tema de colors
-//color_theme.setTheme(config.color_theme);
-var server = http.createServer(function (req, res) {
-    //extrayendo el path de la URL
+    var http = require('http');
+    var colors = require('colors');
+    var fs = require('fs');
+    var mime = require('mime');
+    var path = require('path');
+    var staticServer = require('./Internals/static-server.js');
+    var handlers = require('./Internals/handlers');
+//--cargando mis configuraciones--
+    var config = require('./config/config.js');
+    var IP = config.IP;
+    var PORT = config.PORT;
+//--Tema de mis colores--
+//--color_theme.setTheme(config.color_theme);--
+    var server = http.createServer(function (req, res) {
+//--Extrayendo el path de la URL--
     var urlpath = req.url;
-    // normalizando el path en caso de no se pidea ningun recurso  
+//--Normalizamos el path en caso de que ningun archivo sea requerido--  
     if(urlpath==="/"){
         urlpath =("/index.html");
     }
      if (typeof(handlers[urlpath]) === "function"){
          handlers[urlpath](req,res);
      }else {
-        //se llama al servidor estatico
+//--Se llama al servidor estatico--
         staticServer.serve(urlpath,res);
      }
-    
-
-       
 });
 server.listen(PORT, IP, function () {
     console.log(`> Server working @http://${IP}:${PORT}/`)
